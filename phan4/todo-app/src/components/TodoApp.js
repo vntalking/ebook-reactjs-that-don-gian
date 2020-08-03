b92c30eb25ff67a5ec9ba4227d952d8d70dd7cfb
vntalking <anhson.duong@outlook.com>
2020-08-03 19:19:25 +0700

    Phan 4: Tạo các components

---
+++ phan4/todo-app/src/components/TodoApp.js
@@ -0,0 +1,12 @@
+import React from "react";
+class TodoApp extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello from Create React App</h1>
+                <p>I am in a React Component!</p>
+            </div>
+        );
+    }
+}
+export default TodoApp;
---
+++ phan4/todo-app/src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import TodoApp from "./components/TodoApp";
+
+ReactDOM.render(<TodoApp />, document.getElementById("root"));
