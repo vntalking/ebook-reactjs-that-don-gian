c9e60b3176af82a9baa9deda1078019b7492876a
vntalking <anhson.duong@outlook.com>
2020-08-03 20:03:17 +0700

    Phan 4: Thêm checkbox trong Todo Items

--- phan4/todo-app/src/components/TodoItem.js
+++ phan4/todo-app/src/components/TodoItem.js
@@ -3,7 +3,9 @@ import React from "react";
 class TodoItem extends React.Component {
     render() {
         return (
-            <li className="todo-item">{this.props.todo.title}</li>
+            <li className="todo-item">
+                <input type="checkbox" />{this.props.todo.title}
+            </li>
         );
     }
 }
