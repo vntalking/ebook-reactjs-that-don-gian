43c2cefbae4f807cfe45edfc9428d0e6371b4b3b
vntalking <anhson.duong@outlook.com>
2020-08-03 19:37:58 +0700

    Phan 4: Thêm State

--- phan4/todo-app/src/components/TodoApp.js
+++ phan4/todo-app/src/components/TodoApp.js
@@ -2,10 +2,35 @@ import React from "react";
 import Header from "../components/layout/Header";
 
 class TodoApp extends React.Component {
+    state = {
+        todos: [
+            {
+                id: 1,
+                title: "Setup development environment",
+                completed: true
+            },
+            {
+                id: 2,
+                title: "Develop website and add content",
+                completed: false
+            },
+            {
+                id: 3,
+                title: "Deploy to live server",
+                completed: false
+            }
+        ]
+    };
+
     render() {
         return (
             <div>
                 <Header/>
+                <ul>
+                    {this.state.todos.map(todo => (
+                        <li key={todo.id}>{todo.title}</li>
+                    ))}
+                </ul>
             </div>
         );
     }
